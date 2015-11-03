e734481f794219ee33c3292d7f46c9be27e464a9
Alessandro Lepore <lepore.ale@gmail.com>
2015-11-03 13:30:54 +0100

    check for Order#email presence

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -4,6 +4,7 @@ module Spree
       -> { limit_time = Time.current - SpreeAbandonedCarts::Config.abandoned_after_minutes.minutes
 
            incomplete.
+           where('email IS NOT NULL').
            where("#{quoted_table_name}.item_total > 0").
            where("#{quoted_table_name}.updated_at < ?", limit_time) }
 
