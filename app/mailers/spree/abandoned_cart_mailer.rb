75d2fca34c50e518a73da1dc55a703e582926902
Alessandro Lepore <lepore.ale@gmail.com>
2015-11-03 13:30:54 +0100

    check for Order#email presence

--- app/mailers/spree/abandoned_cart_mailer.rb
+++ app/mailers/spree/abandoned_cart_mailer.rb
@@ -1,9 +1,11 @@
 module Spree
   class AbandonedCartMailer < BaseMailer
     def abandoned_cart_email(order)
-      @order = order
-      subject = "#{Spree::Store.current.name} #{Spree.t(:abandoned_cart_subject)}"
-      mail(to: order.email, from: from_address, subject: subject)
+      if order.email.present?
+        @order = order
+        subject = "#{Spree::Store.current.name} - #{Spree.t(:abandoned_cart_subject)}"
+        mail(to: order.email, from: from_address, subject: subject)
+      end
     end
   end
 end
